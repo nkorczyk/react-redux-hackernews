ae924d0d98962eda9358c0a297ac5c2169cd51d7
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-01-07 15:20:27 +0100

    Added components & and initialized redux store

---
+++ src/components/Story.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Story.css';
+
+const Story = ({ story, columns, onArchive }) => {
+    console.log(columns);
+    const {
+        title,
+        url,
+        author,
+        num_comments,
+        points,
+        objectID
+    } = story;
+
+    return (
+        <div className="story">
+            <span style={{ wiidth: columns.title.width }}>
+                <a href={url}>{title}</a>
+            </span>
+            <span style={{ wiidth: columns.author.width }}>
+                {author}
+            </span>
+            <span style={{ wiidth: columns.comments.width }}>
+                {num_comments}
+            </span>
+            <span style={{ wiidth: columns.points.width }}>
+                {points}
+            </span>
+            <span style={{ width: columns.archive.width }}>
+                <button type="button"
+                    className="button-inline"
+                    onClick={() => onArchive(objectID)}>
+                </button>
+                <ButtonInline type="button"
+                    onClick={() => onArchive(objectID)}>
+                    Archive
+                </ButtonInline>
+            </span>
+        </div>
+    )
+};
+
+const ButtonInline = ({ onClick, type = 'button', children }) => {
+    return (
+        <button type={type}
+            className="button-inline"
+            onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+
+export default Story;
