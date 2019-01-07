24af151700d8b73bd17ff53f28624a59dd129fe1
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-01-07 16:17:55 +0100

    Added redux Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
+import { STORY_ARCHIVE } from './actions/actionTypes';
 
 ReactDOM.render(<App
     stories={store.getState().storyState}
-    onArchive={() => { }} />,
+    onArchive={id => {
+        store.dispatch({ type: STORY_ARCHIVE, id })
+    }} />,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/reducers/archiveReducer.js
+++ src/reducers/archiveReducer.js
@@ -1,9 +1,20 @@
+import { STORY_ARCHIVE } from '../actions/actionTypes';
+
 const INITIAL_STATE = [];
 
 function archiveReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
+        case STORY_ARCHIVE:
+            return applyArchiveStory(state, action);
         default: return state;
     }
+}
+
+const applyArchiveStory = (state, action) => {
+    return [
+        ...state,
+        action.id
+    ];
 }
 
 export default archiveReducer;
